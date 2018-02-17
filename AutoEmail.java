7f04967006de2a08f1c413ab607d930b3ec0ca64
Alvee93 <31181902+Alvee93@users.noreply.github.com>
2018-02-17 22:24:13 +0600

    first release

--- AutoEmail.java
+++ AutoEmail.java
@@ -1,37 +1,34 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AutoEmail {
     public static void main(String[] args) {
-
         String guruUrl = "http://demo.guru99.com/selenium/newtours/";
         String googleUrl = "https://www.google.com/";
         String gmailURL = "https://accounts.google.com/signin/v2/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
 
-        String emailAddr = "webalive.srv@gmail.com";
-        String emailPass = "bitmascot@321"; //whsOnd zHQkBf
+        String emailAddr = "alvee.bit03@gmail.com";
+        String emailPass = "Alvee_123"; //whsOnd zHQkBf emailToAddress
+        String emailToAddress = "tabassumwazed@gmail.com";
+        String emailSubject = "Email Sujbect Text";
+        String emailBody = "This is an automated email using java webdriver. " +
+                "Lorem ipsum dolor sit amet proin gravida. Lorem ipsum dolor sit amet proin gravida. " +
+                "Lorem ipsum dolor sit amet proin gravida. Lorem ipsum dolor sit amet proin gravida. ";
+
         String ePassClass = "whsOnd zHQkBf";
 
         String expectedTitle = "Gmail";
         String actualTitle = "";
 
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Alvee\\Downloads\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", //webdriver.gecko.driver, instead chrome driver
+                "D:\\Softs\\Java_WebDriver\\chromedriver.exe");   // Set Fire fox Driver class path
+        WebDriver driver = new ChromeDriver(); //Create a instance of FirefoxDriver
+            driver.get(gmailURL); //Navigate an url
 
-        WebDriver driver = new FirefoxDriver(); //Create driver object
-
-        //driver.get(guruUrl); //Navigate an url
-
-        //WebElement emailField = driver.findElement(By.className("whsOnd")); //Find an html element object by class name
-
-        //emailField.click(); //mouse click event
-
-        //emailField.sendKeys(“Text you want to set in html element”);
-
-        driver.get(gmailURL);
         actualTitle = driver.getTitle();
 
         if (actualTitle.contentEquals(expectedTitle)){
@@ -45,6 +42,12 @@ public class AutoEmail {
         WebElement NxtBtn = driver.findElement(By.id("identifierNext"));
         NxtBtn.click();
 
+        try {
+            Thread.sleep(3000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
         WebElement PassField = driver.findElement(By.name("password")); //whsOnd zHQkBf
 
         WebDriverWait wait = new WebDriverWait(driver, 20);
@@ -52,24 +55,35 @@ public class AutoEmail {
         PassField.sendKeys(emailPass);
         driver.findElement(By.id("passwordNext")).click();
 
-        /*PassField.sendKeys("bitmascot@321");
-        System.out.println("Debug: After inserting the password");
-        WebElement PassNxtBtn = driver.findElement(By.id("passwordNext"));
-        PassNxtBtn.click(); */
-
-        System.out.println(driver.getTitle());
+        System.out.println(driver.getTitle() + " and the password inserted" );
 
         WebDriverWait wait1 = new WebDriverWait(driver, 15);
-        WebElement element1 = wait1.until(ExpectedConditions.elementToBeClickable(By.className("T-I J-J5-Ji T-I-KE L3"))); //T-I J-J5-Ji T-I-KE L3 T-I-J0 T-I-JW      L3 T-I-J0
+        WebElement element1 = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath(".//div[text()='COMPOSE']")));
+        //T-I J-J5-Ji T-I-KE L3 T-I-J0 T-I-JW      L3 T-I-J0  T-I J-J5-Ji T-I-KE L3
         element1.click();
 
-        /* WebElement comField = driver.findElement(By.className("T-I J-J5-Ji T-I-KE L3"));
-        System.out.println("selected compose button");
-        WebDriverWait wait1 = new WebDriverWait(driver, 20);
-        wait1.until(ExpectedConditions.elementToBeClickable(comField));
-        driver.findElement(By.className("T-I J-J5-Ji T-I-KE L3")).click(); */
+        try {
+            Thread.sleep(9000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        driver.findElement(By.xpath(".//textarea[@class='vO' and @name='to']")).sendKeys(emailToAddress); // //*[@id=":lb"] instead of //*[@id=':lb']
+        System.out.println("Inserted to address");
+        driver.findElement(By.xpath(".//input[@name='subjectbox']")).sendKeys(emailSubject);
+        System.out.println("Inserted to subject");
+        driver.findElement(By.xpath(".//div[@class='Am Al editable LW-avf']")).sendKeys(emailBody);
+        System.out.println("Inserted to email body");
+
+        driver.findElement(By.xpath(".//div[@data-tooltip='Send \u202A(Ctrl-Enter)\u202C']")).click();
+
+        try {
+            Thread.sleep(1000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Email sent");
 
         driver.close();
-
     }
 }
