111beec450e2128f2a4b0689f072b405792ff99c
Alvee93 <31181902+Alvee93@users.noreply.github.com>
2018-07-12 06:48:27 +0600

    Update AutoEmail.java

--- AutoEmail.java
+++ AutoEmail.java
@@ -13,7 +13,7 @@ public class AutoEmail {
         String gmailURL = "https://accounts.google.com/signin/v2/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
 
         String emailAddr = "webalive.srv@gmail.com";
-        String emailPass = "bitmascot@321"; //whsOnd zHQkBf
+        String emailPass = "pass_here"; //whsOnd zHQkBf
         String ePassClass = "whsOnd zHQkBf";
 
         String expectedTitle = "Gmail";
